ad0995a719fc6823c3fb316efd9c3d227b2dd58b
Ahmad Mustafa <ahmadmustafabutt01@gmail.com>
2026-04-19 14:16:56 +0500

    chore: rename project, clean up vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,11 @@ export default defineConfig({
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
   },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
 });
